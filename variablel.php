efb096d7206fd78d4180e1d1bead26851b088a9c
yasuko_makino <makiyasu2411@gmail.com>
2023-05-23 14:46:53 +0900

    適当なコメント文

---
+++ variablel.php
@@ -0,0 +1,20 @@
+<?php
+$greeting = "こんにちは";
+?>
+
+<?php
+$greeting = "こんにちは";
+echo $greeting;
+?>
+
+<?php
+$greeting = "こんばんは";
+echo $greeting;
+?>
+
+echo "<pre>"
+var_dump($greeChugoku);
+var_dump(true);
+var_dump( 1 > 2 );
+var_dump( $greeting == "こんばんば" );
+
